ceb0095e89a05906df637b0a6f4dfc3827c6d2f4
BastThePM <152586621+TheRealBastioul@users.noreply.github.com>
2025-02-27 13:18:17 -0500

    ughhhh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  output: 'export',
+  // ...other valid configuration settings...
 };
 
 export default nextConfig;
